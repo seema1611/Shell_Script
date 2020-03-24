b2e9e44fc92e8b4235d8c56d87629c71527afa81
seema1611 <seemarajpure16@gmail.com>
2020-03-24 15:35:59 +0530

    ADDED: All Linux Commands

---
+++ Linux commands/PipeAndGrepCommands.sh
@@ -0,0 +1,13 @@
+find . -size +5M
+find . -name *.pdf -size +5M
+find . -size +5M | grep pdf
+find . -name data*
+grep -r CAPTAIN linux-content/data.csv
+grep -r CAPTAIN .
+ls -p | grep /
+ls -p | grep -v /
+
+
+
+
+
