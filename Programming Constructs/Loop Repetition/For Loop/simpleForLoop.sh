949eebccd942e431b32c9e83655eecd17c488931
seema1611 <seemarajpure16@gmail.com>
2020-03-25 09:23:35 +0530

    ADDED: Simple For Loop program

---
+++ Programming Constructs/Loop Repetition/For Loop/simpleForLoop.sh
@@ -0,0 +1,11 @@
+#!/bin/bash -x
+
+#Simple Program of how to Working For Loop
+
+#Initialize for Loop
+for(( counter=1; counter<=5; counter++ ))
+do
+        echo -n "$counter "
+done                                            #End Of Loop
+printf '\n'
+
