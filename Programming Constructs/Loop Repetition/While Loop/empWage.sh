33436a26dc456071eceba89e08d1abf7c7d40654
seema1611 <seemarajpure16@gmail.com>
2020-03-25 09:27:07 +0530

    ADDED: Calculate Employee wage using while loop

---
+++ Programming Constructs/Loop Repetition/While Loop/empWage.sh
@@ -0,0 +1,32 @@
+#!/bin/bash -x
+
+#Program to Findout Salary Depending uop Working Hours
+isPartTime=1
+isFullTime=2
+maxHrsInMonth=100
+empRatePerHr=20
+numWorkingDays=20
+
+totalEmpHrs=0;
+totalWorkingDays=0
+
+while [[ $totalEmpHrs -lt $maxHrsInMonth && $totalWorkingDays -lt $numWorkingD>
+do
+        (( totalWorkingDays++))
+        empCheck=$((RANDOM%3))
+                case $empCheck in
+                        $isFullTime)
+                                empHrs=8
+                                 ;;
+                        $isPartTime)
+                                empHrs=4
+                                 ;;
+
+                        *)
+                                empHrs=0
+                                 ;;
+
+                esac
+                totalEmpHrs=$(($totalEmpHrs+$empHrs))
+done
+totalSalary=$((totalEmpHrs*$empRatePerHr))
