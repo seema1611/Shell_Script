8f3b5b11bdb7a651fe124ff9b524d20545f7c6b7
seema1611 <seemarajpure16@gmail.com>
2020-03-27 23:12:06 +0530

    ADDED: Simple Dectionary Program

---
+++ Dictionary Example/simpleDictionaries.sh
@@ -0,0 +1,14 @@
+#! /bin/bash -x
+
+declare -A sounds
+
+sounds[dog]="bark"
+sounds[cow]="moo"
+sounds[bird]="tweet"
+sounds[wolf]="howl"
+
+echo "Dog Sound "${sounds[dog]}           #Dog's sound
+echo "All animal Sound "${sounds[@]}      #All values
+echo "Animal "${!sounds[@]} 		  #All keys
+echo "Number of Animal " ${#sounds[@]}    #Number of elements
+unset sounds[dog]			  #Delete dog
